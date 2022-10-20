2332c9b2c76e14d60c9db59db4849c02b149c1f7
tushariccs <103886040+tushariccs@users.noreply.github.com>
2022-10-20 20:45:55 +0530

    Add files via upload

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -2,10 +2,66 @@
 var NoofDrums  = document.querySelectorAll(".drum").length;
 
 for (let i = 0; i < NoofDrums; i++) {
-    document.querySelectorAll("button")[i].addEventListener("click", function () {
+    document.querySelectorAll("button")[i].addEventListener("click", function () {  //(parameter) => i.e function() is just 
+        //repalced by () =>i.e anonymous function
         var option = this.innerHTML; 
-        console.log(option)
-        switch (option)
+        makeSound(option);
+    //     console.log(option)
+    //     switch (option)
+    //     {
+    //         case "w":
+    //             var audio = new Audio("sounds/tom-1.mp3");
+    //             audio.play();
+    //             break;
+    //         case "a":
+    //             var audio = new Audio("sounds/tom-2.mp3");
+    //             audio.play();
+    //             break;
+    //         case "s":
+    //             var audio = new Audio("sounds/tom-3.mp3");
+    //             audio.play();
+    //             break;
+    //         case "d":
+    //             var audio = new Audio("sounds/tom-4.mp3");
+    //             audio.play();
+    //             break;
+    //         case "d":
+    //             var audio = new Audio("sounds/tom-4.mp3");
+    //             audio.play();
+    //             break;
+    //         case "j":
+    //             var audio = new Audio("sounds/snare.mp3");
+    //             audio.play();
+    //             break;
+    //         case "k":
+    //             var audio = new Audio("sounds/crash.mp3");
+    //             audio.play();
+    //             break;
+    //         case "l":
+    //             var audio = new Audio("sounds/kick-bass.mp3");
+    //             audio.play();
+    //             break;
+    //         default:
+    //             console.log("Button imporriate");
+    //     }
+    });
+}
+    document.addEventListener("keypress", function (event) {
+        //alert("Key pressed");
+   
+        makeSound(event.key);
+        
+        //O/p
+        //index.js:52 KeyboardEvent {isTrusted: true, key: 'w', code: 'KeyW', location: 0, ctrlKey: false, …}isTrusted: truealtKey: falsebubbles: truecancelBubble: falsecancelable: truecharCode: 119code: "KeyW"composed: truectrlKey: falsecurrentTarget: nulldefaultPrevented: falsedetail: 0eventPhase: 0isComposing: falsekey: "w"keyCode: 119location: 0metaKey: falsepath: (4) [body, html, document, Window]repeat: falsereturnValue: trueshiftKey: falsesourceCapabilities: InputDeviceCapabilities {firesTouchEvents: false}srcElement: bodytarget: bodytimeStamp: 437.89999997615814type: "keypress"view: Window {window: Window, self: Window, document: document, name: '', location: Location, …}which: 119[[Prototype]]: KeyboardEvent
+        // index.js:52 KeyboardEvent {isTrusted: true, key: 'a', code: 'KeyA', location: 0, ctrlKey: false, …}
+        // index.js:52 KeyboardEvent {isTrusted: true, key: 's', code: 'KeyS', location: 0, ctrlKey: false, …}
+        // index.js:52 KeyboardEvent {isTrusted: true, key: 'd', code: 'KeyD', location: 0, ctrlKey: false, …}
+    });
+
+
+
+function makeSound(key)
+{     switch (key)
         {
             case "w":
                 var audio = new Audio("sounds/tom-1.mp3");
@@ -42,8 +98,8 @@ for (let i = 0; i < NoofDrums; i++) {
             default:
                 console.log("Button imporriate");
         }
-    });
-}
+    
+    }
 
 
 // document.querySelector("button").addEventListener("click", function () {
